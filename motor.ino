0d40a7222bcc4fb42a2a30276f27cb8c4fa6dde9
Panyaphat Pipatsaraphan <p.pipatsaraphan@gmail.com>
2024-06-04 14:09:47 +0700

    Last Commit

--- motor.ino
+++ motor.ino
@@ -1,31 +1,32 @@
-int motor1pin1 = 4;
-int motor1pin2 = 5;
-
-int motor2pin1 = 6;
-int motor2pin2 = 7;
-
 void forward(){
-  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
 
-  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin1, LOW);
   digitalWrite(motor2pin2, HIGH);
 }
 void turnLeft(){
   digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor1pin2, HIGH);
 
   digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, HIGH);
+  digitalWrite(motor2pin2, LOW);
 }
 void turnRight(){
   digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor1pin2, LOW);
 
   digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, LOW);
+  digitalWrite(motor2pin2, HIGH);
 }
 void backward(){
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+}
+void stop(){
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, LOW);
 
